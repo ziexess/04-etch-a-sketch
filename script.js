6c3e8a7ce8a902bbc1dd4975c16dc195ec2f33cf
ziexes <ahmed.akrabuwu@gmail.com>
2024-05-01 08:46:56 +0300

    Add hover effect

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ for (let i = 0; i < 16; i++) {
     container.appendChild(row);
     for (let j = 0; j < 16; j++) {
         const col = document.createElement("div");
-        col.classList.add("col")
+        col.classList.add("col");
         row.appendChild(col);
     }
 }
+
+const grid = document.querySelectorAll(".row, .col")
+grid.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+        // We can add class instead here
+        div.style.backgroundColor = "black";
+    })
+})
